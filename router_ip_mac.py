590aebf898ee95e263b2c400868cd985810c34e9
Brunotti12 <128841706+Brunotti12@users.noreply.github.com>
2024-11-07 20:06:23 +0100

    Update router_ip_mac.py

--- router_ip_mac.py
+++ router_ip_mac.py
@@ -1,13 +1,13 @@
 import subprocess
 import re
 
-def get_router_ip_mac():
+def get_router_ip():
     try:
         result = subprocess.check_output("arp -n", shell=True).decode()
-        i = 0
-        while(i < len(result)):
-            if(result[i + 91:i + 95] == '.1  '):
-                return (result[i + 81: i + 93], result[i + 114:i + 131])
-            i += 80
+
+        for i in result.splitlines():
+            for j in i.split():
+                if j[-2:] == '.1':
+                    return j
     except subprocess.CalledProcessError:
-        return "failed to retrieve table"
+        return "couldn't fetch arp table"
