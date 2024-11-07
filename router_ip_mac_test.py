c548bcb48f421f153c9eb07d742dd625f3f1e30e
Brunotti12 <128841706+Brunotti12@users.noreply.github.com>
2024-11-07 22:58:11 +0100

    Update and rename router_ip_test.py to router_ip_mac_test.py

---
+++ router_ip_mac_test.py
@@ -0,0 +1,9 @@
+import router_ip_mac
+
+print("Router ip = ", router_ip_mac.get_router_ip())
+
+try:
+    ip = input("Geef het ip adress: ")
+    print("MAC address: ", router_ip_mac.get_mac(ip))
+except KeyboardInterrupt:
+    print("\nTerminated")
--- router_ip_test.py
+++
@@ -1,3 +0,0 @@
-import router_ip_mac
-
-print(router_ip_mac.get_router_ip())
